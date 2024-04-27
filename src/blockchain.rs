df7640e018f5327f07f1a4819c3770a623448571
nulltrek <nulltrek@users.noreply.github.com>
2024-04-27 12:22:49 +0200

    Add query_tx function to blockchain

--- src/blockchain.rs
+++ src/blockchain.rs
@@ -1,6 +1,7 @@
 use crate::block::Block;
 use crate::hash::Hash;
 use crate::io::{ByteIO, FileIO};
+use crate::transaction::Transaction;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug)]
@@ -40,10 +41,21 @@ impl Blockchain {
         Err(BlockchainError::new("Cannot add block to chain"))
     }
 
-    pub fn query(&self, hash: &Hash) -> Option<(usize, &Block)> {
+    pub fn query_block(&self, hash: &Hash) -> Option<(usize, &Block)> {
         for (i, block) in self.list.iter().enumerate().rev() {
             if block.hash == *hash {
                 return Some((i, &block));
+            }
+        }
+        return None;
+    }
+
+    pub fn query_tx(&self, hash: &Hash) -> Option<(usize, &Transaction)> {
+        for (i, block) in self.list.iter().enumerate().rev() {
+            for tx in block.data.transactions.iter() {
+                if tx.hash == *hash {
+                    return Some((i, &tx));
+                }
             }
         }
         return None;
@@ -138,36 +150,65 @@ mod tests {
     fn add_block_height() {
         let mut block_gen = BlockGen::new(true);
 
-        let mut chain = Blockchain::new(&block_gen.next().unwrap());
+        let mut chain = Blockchain::new(block_gen.next().unwrap());
         assert_eq!(chain.height(), 1);
 
-        let index = chain.append(&block_gen.next().unwrap()).unwrap();
+        let index = chain.append(block_gen.next().unwrap()).unwrap();
         assert_eq!(chain.height(), 2);
         assert_eq!(index, 1);
 
-        let index = chain.append(&block_gen.next().unwrap()).unwrap();
+        let index = chain.append(block_gen.next().unwrap()).unwrap();
         assert_eq!(chain.height(), 3);
         assert_eq!(index, 2);
     }
 
     #[test]
-    fn querying() {
+    fn query_blocks() {
         let mut block_gen = BlockGen::new(true);
 
-        let mut chain = Blockchain::new(&block_gen.next().unwrap());
+        let mut chain = Blockchain::new(block_gen.next().unwrap());
         let mut hashes = Vec::<(Hash, usize)>::new();
         for _ in 0..9 {
             let block = block_gen.next().unwrap();
-            let index = chain.append(&block).unwrap();
-            hashes.push((block.hash.clone(), index));
+            let block_hash = block.hash.clone();
+            let index = chain.append(block).unwrap();
+            hashes.push((block_hash, index));
         }
 
         for (hash, index) in hashes {
-            let (height, _) = chain.query(&hash).unwrap();
+            let (height, _) = chain.query_block(&hash).unwrap();
             assert_eq!(height, index);
         }
 
-        let result = chain.query(&Hash::new(b"nothing"));
+        let result = chain.query_block(&Hash::new(b"nothing"));
+        assert!(result.is_none());
+    }
+
+    use rand::seq::SliceRandom;
+    use rand::thread_rng;
+
+    #[test]
+    fn query_txs() {
+        let mut rng = thread_rng();
+        let mut block_gen = BlockGen::new(true);
+
+        let mut chain = Blockchain::new(block_gen.next().unwrap());
+        let mut hashes = Vec::<(Hash, usize)>::new();
+        for _ in 0..9 {
+            let block = block_gen.next().unwrap();
+
+            let tx = block.data.transactions.choose(&mut rng).unwrap();
+            let tx_hash = tx.hash.clone();
+            let height = chain.append(block).unwrap();
+            hashes.push((tx_hash, height));
+        }
+
+        for (hash, index) in hashes {
+            let (height, _) = chain.query_tx(&hash).unwrap();
+            assert_eq!(height, index);
+        }
+
+        let result = chain.query_tx(&Hash::new(b"nothing"));
         assert!(result.is_none());
     }
 }
